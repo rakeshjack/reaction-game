96f594d8f61bfd1b7aa306396c356d3f30ae1079
rakeshAkhila <rakesh.r@2basetechnologies.com>
2021-10-05 11:21:55 +0530

    Initial commit for reaction-timer

--- Tuts/modal-project/src/App.vue
+++ Tuts/modal-project/src/App.vue
@@ -1,21 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{title}}</h1>
+  <input type="text" ref="name"><br>
+  <button @click="handleClick">Click me</button><br>
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="" @close="toggleModal">
+        <template v-slot:links>
+            <a href="#">Sign up now</a><br><br>
+            <br>
+            <a href="#">more info</a>
+        </template>
+
+        <h1>Ninja Giveaway</h1>
+        <p>Grab your Ninja swag for half price</p>
+    </Modal>
+  </teleport>
+  <teleport to=".modals" v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+        <template v-slot:lists>
+            <ul>
+              <li>India</li>
+              <li>Pakistan</li>
+              <li>China</li>
+              <li>Indonsia</li>
+              <li>Japan</li>
+            </ul>
+        </template>
+      <h1>Greetings Humans</h1>
+    </Modal>
+  </teleport>
+  <button @click="toggleModal">Open Modal</button>
+  <button @click="toggleModalTwo">Open Modal 2</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Modal from './components/Modal';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+    return  {
+      title: 'My First Vue.js App :)',
+      header: 'Sign up for the give away',
+      text: 'Grab your ninja swag for half a price',
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add('active');
+      this.$refs.name.focus();
+    },
+
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    }
+
   }
 }
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ Tuts/modal-project/src/components/Modal.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{sale: theme === 'sale'}">
+            <slot></slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
+            <div class="actions">
+                <slot name="lists"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ['theme'],
+    methods: {
+        closeModal() {
+            this.$emit('close');
+        }
+    }
+}
+</script>
+
+<style>
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+
+    .backdrop {
+        top:0;
+        position:fixed;
+        background: rgba(0,0,0,0.5);
+        width:100%;
+        height: 100%;
+    }
+    h1 {
+        color:#03cfb4;
+        border:none;
+        padding:0;
+    }
+    
+    .sale {
+        background: crimson;
+        color: white;
+    }
+
+    .sale h1 {
+        color:white;
+        background: crimson;
+    }
+    .modal .actions {
+        text-align: center;
+        margin:30px 0 10px 0;
+    }
+
+    .modal .actions a {
+        padding: 8px;
+        border: 1px solid black;
+        border-radius: 4px;
+        text-decoration: none;
+        margin:10px;
+    }
+</style>
